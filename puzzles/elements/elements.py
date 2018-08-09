39d394de664bbd9a475ec58404cbbffd8a4bfa89
Belen Barbed Martin <bbarbed@amazon.com>
2018-08-09 21:52:10 +0100

    wrapped periodic table puzzle

--- puzzles/elements/elements.py
+++ puzzles/elements/elements.py
@@ -3,31 +3,33 @@ import random
 import re
 import argparse
 
-parser = argparse.ArgumentParser(description='The periodic table puzzle.')
-parser.add_argument('-d', '--debug', default=False, action='store_true', dest='debug', help='enable debug mode')
-args = parser.parse_args()
+def elementPuzzle(debug=False):
+    with open('words.txt', 'r') as f:
+        words = [line.strip() for line in f]
 
-with open('words.txt', 'r') as f:
-    words = [line.strip() for line in f]
-# print(words)
-# print(len(words))
+    # word to use for code
+    word = words[random.randint(0, len(words))]
+    print('-- ACCESS POD MAINFRAME --\nUser: ' + word.upper())
 
-# word to use for code
-word = words[random.randint(0, len(words))]
-# print(word)
-print('-- ACCESS POD MAINFRAME --\nUser: ' + word.upper())
+    elements = re.findall(r'([A-Z][a-z]*)', word)
 
-elements = re.findall(r'([A-Z][a-z]*)', word)
-# print(elements)
+    code = ''
+    for item in elements:
+        code += str(element(item).atomic_number)
 
-code = ''
-for item in elements:
-    code += str(element(item).atomic_number)
+    if debug: print(code)
 
-if args.debug: print(code)
+    attempt = ''
+    while attempt != code:
+        attempt = input('Password: ')
 
-attempt = ''
-while attempt != code:
-    attempt = input('Password: ')
+    print('Sign-in successful!')
 
-print('Sign-in successful!')
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='The periodic table puzzle.')
+    parser.add_argument('-d', '--debug', default=False, action='store_true', dest='debug', help='enable debug mode')
+    args = parser.parse_args()
+
+    elementPuzzle(debug=args.debug)
